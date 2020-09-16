2fe42250752ed917d5bc21644cfa78737ec28f42
Raven <raven025@outlook.com>
2020-09-16 20:50:12 +0800

    添加了Dataset与DataLoder，初步构建好模型

---
+++ model.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from util import getDFTFeature
+
+class SELDNet(nn.Module):
+    def __init__(self,C=4,P=16,Q=16,R=16,K=10,feature_dim=512,cnn_layers=3,rnn_layers=2):
+        super().__init__()
+        self.C = C # the number of audio channels
+        self.P = P # the number of feature maps
+        self.Q = Q # the number of RNN hidden units
+        self.R = R # the dimension of first full connection
+        self.K = K # the number of classes
+        self.feature_dim = feature_dim # the dimension of input features
+        self.cnn_layers = cnn_layers # the number of CNN layers
+        self.rnn_layers = rnn_layers # the number of RNN layers
+        ##########################################
+        # CNN part
+        ##########################################
+        self.convs = [nn.Conv2d(in_channels=2*C,out_channels=P,
+                        kernel_size=(3,3),padding=(1,1))]
+        for _ in range(cnn_layers-1):
+            self.convs.append(nn.Conv2d(in_channels=P,out_channels=P,
+                        kernel_size=(3,3),padding=(1,1)))
+
+        # name all layers
+        for i, conv in enumerate(self.convs):
+            self.add_module("conv{}".format(i), conv)
+            self.add_module("bn{}".format(i), nn.BatchNorm2d(P, affine=False))
+
+
+        ##########################################
+        # RNN part
+        ##########################################
+        self.grus = nn.GRU(input_size=2*P,hidden_size=Q,num_layers=rnn_layers,batch_first=True,bidirectional=True)
+        self.add_module("grus",self.grus)
+
+
+        ##########################################
+        # SED part
+        ##########################################
+        self.sed_fc0 = nn.Linear(2*Q,R)
+        self.sed_fc1 = nn.Linear(R,K)
+        self.add_module("sed_fc0",self.sed_fc0)
+        self.add_module("sed_fc1",self.sed_fc1)
+
+        ##########################################
+        # DOA part
+        ##########################################
+        self.doa_fc0 = nn.Linear(2*Q,R)
+        self.doa_fc1 = nn.Linear(R,3*K)
+        self.add_module("doa_fc0",self.doa_fc0)
+        self.add_module("doa_fc1",self.doa_fc1)
+
+
+        
+        
+    
+    def forward(self,x):
+        # x shape (N,2*C,T,M)
+        for i in range(self.cnn_layers):
+            y = F.relu(getattr(self,"conv{}".format(i))(x))
+            if i<self.cnn_layers-1:
+                y = F.max_pool2d(getattr(self,"bn{}".format(i))(y),kernel_size=(1,7))
+            else:
+                y = F.max_pool2d(getattr(self,"bn{}".format(i))(y),kernel_size=(1,5))
+            x = y
+        # final y shape (N,P,T,2)
+
+        y = y.permute(0,2,3,1) # shape(N,T,2,P)
+        y = y.contiguous().view(y.shape[0],y.shape[1],-1) # shape (N,T,2*P)
+        h0 = torch.rand((2*self.rnn_layers,y.shape[0],self.Q))
+        y, _ = self.grus(y,h0) # shape(N,T,2*Q)
+        y = torch.tanh(y) # shape(N,T,2*Q)
+
+        sed_y = torch.sigmoid(self.sed_fc1(self.sed_fc0(y)))
+        doa_y = torch.tanh(self.doa_fc1(self.doa_fc0(y)))
+        return sed_y, doa_y
+if __name__ == "__main__":
+    tmp = SELDNet()
+
+    FILENAME = "data/mic_dev/split1_ir1_ov1_24.wav"
+    feature = getDFTFeature(FILENAME)
+    print(feature.shape)
+    feature = feature.unsqueeze(0)
+    output = tmp(feature)
+    print(output[0].shape)
+    print(output[1].shape)
+    print(output[1])
+
--- util.py
+++ util.py
@@ -1,10 +1,12 @@
 import torchaudio
 import torch
+import torch.utils.data as data
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-def getDFTFeature(filepath,win_size=1024,win_shift=512,preemphasis=False):
+import os
+def getDFTFeature(filepath,win_size=1024,win_shift=512,preemphasis=False,channel_first=True,drop_dc=True,cut_len=5000):
     '''
     获取一个音频的对数DFT频谱
     
@@ -13,6 +15,9 @@ def getDFTFeature(filepath,win_size=1024,win_shift=512,preemphasis=False):
         win_size: 窗口大小（点）
         win_shift: 滑动距离（点）
         preemphasis: 是否预强化。通过一阶差分弱低频强高频
+        channel_first: whether to put channels in first dimension
+        drop_dc: whether to drop DC component in spectrum (frequency==0)
+        cut_len: keep the fix number of points in time axis
         
     Return:
         (log_power_spectrum, phase_spectrum):能量谱与相位谱相叠形成的tensor
@@ -41,7 +46,9 @@ def getDFTFeature(filepath,win_size=1024,win_shift=512,preemphasis=False):
     win_func = torch.hamming_window(win_size,periodic=False)
     windowed_input = strided_input * win_func
     fft = torch.rfft(windowed_input,1,normalized=False, onesided=True)*2/win_size
-    fft = fft[:,:,1:]
+    if drop_dc:
+        fft = fft[:,:,1:]
+    fft = fft[:,:cut_len,:]
     power_spectrum = fft.pow(2).sum(3)
     log_power_spectrum = np.log10(power_spectrum)*10
     phase_spectrum = fft[:,:,:,0]/fft.pow(2).sum(3).sqrt()
@@ -49,12 +56,32 @@ def getDFTFeature(filepath,win_size=1024,win_shift=512,preemphasis=False):
     phase_spectrum = torch.acos(phase_spectrum)
     phase_spectrum[fft[:,:,:,0]<0] = -phase_spectrum[fft[:,:,:,0]<0]
     spectrums = torch.cat([log_power_spectrum,phase_spectrum],dim=0)
-    return spectrums.permute(1,2,0)
+    if not channel_first:
+        spectrums = spectrums.permute(1,2,0)
+    return spectrums
+
+class TUTDataset(data.Dataset):
+    '''
+    TUT audio dataset
+    '''
+    def __init__(self, data_folder):
+        '''
+        Args:
+            data_folder: the path where audio files are stored in
+        '''
+        super(TUTDataset).__init__()
+        self.data_folder = data_folder
+        self.file_names = list(os.listdir(data_folder))
+
+    def __getitem__(self,index):
+        file_name = self.file_names[index]
+        return getDFTFeature(os.path.join(self.data_folder,file_name))
+
+    def __len__(self):
+        return len(self.file_names)
 
 if __name__ == "__main__":
-    FILENAME = "data/mic_dev/split1_ir1_ov1_24.wav"
-    feature = getDFTFeature(FILENAME)
-    plt.figure()
-    plt.imshow(feature[:,:,0].T,aspect='auto')
-    plt.savefig('test.png')
-    
+    tutdata = TUTDataset("data/mic_dev")
+    loader = data.DataLoader(tutdata,batch_size=8)
+    for v in loader:
+        print(v.shape)
