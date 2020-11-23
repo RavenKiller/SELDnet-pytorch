af7644be9f45594723d684b68bc5c59996e15e48
Raven <raven025@outlook.com>
2020-11-23 08:55:02 +0800

    store

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import torch.functional as F
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
-
+from tensorboardX import SummaryWriter
 from util import TUTDataset
 from model import SELDNet
 from pprint import pprint
@@ -26,16 +26,18 @@ def train(args):
             batch_size
             epoch_num
     '''
+    writer = SummaryWriter()
+
     tutdata = TUTDataset("data/mic_dev","data/metadata_dev",sample_freq=44100,split_set=SPLIT_SET,ir_set=IR_SET,ov_set=OV_SET)
     tutloader = data.DataLoader(tutdata,batch_size=args.batch_size,shuffle=True)
     evaldata = TUTDataset("data/mic_dev","data/metadata_dev",sample_freq=44100,split_set=["split3"],ir_set=IR_SET,ov_set=OV_SET)
     evalloader = iter(data.DataLoader(evaldata,batch_size=args.batch_size,shuffle=False))
     sample_eval, sed_eval, doa_eval = next(evalloader)
     # !!已验证单解码函数在eval数据上工作正常
-    tmp = torch.zeros((4,5160,12)).scatter_(2,sed_eval.unsqueeze(2),1)
-    print(tmp.shape)
-    print(sed_eval.shape,doa_eval.shape)
-    pprint(tutdata.decode_one(tmp[0],doa_eval[0]))
+    # tmp = torch.zeros((4,5160,12)).scatter_(2,sed_eval.unsqueeze(2),1)
+    # print(tmp.shape)
+    # print(sed_eval.shape,doa_eval.shape)
+    # pprint(tutdata.decode_one(tmp[0],doa_eval[0]))
     sample_eval = sample_eval.to(device)
     sed_eval = sed_eval.to(device)
     doa_eval = doa_eval.to(device)
@@ -65,9 +67,10 @@ def train(args):
             out_sed = out_sed.reshape(-1,tutdata.num_class)
             sed = sed.reshape(-1)
             loss_sed = criterion_sed(out_sed,sed)
-            loss_doa = criterion_doa(out_doa.double(),doa.double())
-            loss = loss_sed+loss_doa
+            # ??loss_doa = criterion_doa(out_doa.double(),doa.double())
+            loss = loss_sed # ??+loss_doa
             train_loss_sum += float(loss)
+            writer.add_scalar("train_loss",float(loss),global_step=steps)
             loss.backward()
             nn.utils.clip_grad_norm_(model.parameters(),2) # 防止梯度爆炸
             optimizer.step()
@@ -81,8 +84,9 @@ def train(args):
                     out_sed = out_sed.reshape(-1,tutdata.num_class)
                     sed_eval = sed_eval.reshape(-1)
                     loss_sed = criterion_sed(out_sed,sed_eval)
-                    loss_doa = criterion_doa(out_doa.double(),doa_eval.double())
-                    loss = loss_sed+loss_doa
+                    # ??loss_doa = criterion_doa(out_doa.double(),doa_eval.double())
+                    loss = loss_sed # ??+loss_doa
+                    writer.add_scalar("eval_loss",float(loss),global_step=steps)
                     print("train loss: {}, evaluation loss: {}".format(float(train_loss_sum)/save_step,float(loss)))
                     if float(loss)<min_loss:
                         print("saveing model...")
@@ -92,7 +96,7 @@ def train(args):
                 model.train()
 
             steps+=1
-
+    writer.close()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SELDNet params')
     parser.add_argument('--epoch_num', type=int,default=4,help='The number of total training epochs')
